7bd33a2747932d3e2ecd6772533e47f2c0941bc2
tuna2134 <contact@tuna2134.dev>
2024-04-24 22:26:16 +0900

    add shadow

--- components/pages/page.tsx
+++ components/pages/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ImageCard: React.FC = () => {
   return (
-    <div className="border rounded-md p-2">
+    <div className="border rounded-md p-2 shadow">
       <Image src="/React.png" alt="React" width={782} height={400} />
       <p className="mt-2 text-center text-slate-700">React</p>
     </div>
