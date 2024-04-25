93f82487825e6d387d5a889bfcb9993cde52485b
コマリン親衛隊 <contact@tuna2134.dev>
2024-04-26 01:56:16 +0900

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,8 +5,9 @@ export const Header: React.FC = () => {
     <header className="h-14 border-b shadow-md animate-fade-in-top top-0 sticky bg-white">
       <div className="ui-template flex items-center h-full">
         <h1 className="text-2xl">ServiceLogos</h1>
-        <nav className="ml-auto">
+        <nav className="ml-auto space-x-4">
           <Link href="/">Home</Link>
+          <Link href="https://github.com/SAWARATSUKI/ServiceLogos">GitHub</Link>
         </nav>
       </div>
     </header>
