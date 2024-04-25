6ae127361a0bcd6af461f2d7b3639997fe2fd040
コマリン親衛隊 <contact@tuna2134.dev>
2024-04-25 11:46:59 +0900

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header: React.FC = () => {
   return (
-    <header className="h-14 border-b shadow-md animate-fade-in-top top-0 sticky">
+    <header className="h-14 border-b shadow-md animate-fade-in-top top-0 sticky bg-white">
       <div className="ui-template flex items-center h-full">
         <h1 className="text-2xl">ServiceLogos</h1>
         <nav className="ml-auto">
